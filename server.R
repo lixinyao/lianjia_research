fa92494639d77dd6ccde5347b00535e76d4cf542
lixinyao <slylixinyao@gmail.com>
2016-04-25 16:56:03 +0800

    数据可筛选，图标可出

--- server.R
+++ server.R
@@ -11,18 +11,66 @@ con1 = dbConnect(RMySQL::MySQL(),
                  host = '127.0.0.1')# 设置连接
 res1 = dbSendQuery(con1,"select 城市,录入时间2 as 录入日期,委托类型,
                    count(房源编号) as 委托量,
-                   avg(总价) as 平均委托价格,avg(建筑面积) as 平均建筑面积
-                   from se房源
+                   avg(case when 建筑面积 > '4' and 建筑面积 < '50000'
+                       and 建成年代 > '1900'
+                       and 室数 < '9'
+                       and 总价/建筑面积 > '10000' and 总价/建筑面积 < '10000'
+                       then 总价/建筑面积 else 0 end) as 单位面积委托价格,
+                   avg(case when 建筑面积 > '4' and 建筑面积 < '50000' 
+                       and 建成年代 > '1900'
+                       and 居数 < '9'
+                       then 建筑面积 else 0 end) as 平均建筑面积
+                   from SE房源
                    group by 城市,录入日期,委托类型
                    order by 城市,录入日期;")# 查询
-mydata = dbFetch(res1,n=-1)
+mydata1 = dbFetch(res1,n=-1)
 server = function(input,output){
-  output$table = DT::renderDataTable(DT::datatable({
-    data = mydata
-    data = subset(data,data$城市==input$select1)
-    data = subset(data,data$录入日期 >= input$dates1[1] &
-                  data$录入日期 < input$dates1[2])
-    data = subset(data,data$委托类型==input$select2)
-    data = subset(data,select=input$indicators1)
+  output$table1 = DT::renderDataTable(DT::datatable({
+    data1 = mydata1
+    data1 = subset(data1,data1$城市==input$select1)
+    data1$录入日期 = as.Date(data1$录入日期,"%Y-%m-%d")
+    data1 = subset(data1,data1$录入日期 >= input$dates1[1] &
+                     data1$录入日期 < input$dates1[2])
+    data1 = subset(data1,data1$委托类型==input$select2)
+    data1 = subset(data1,select=input$indicators1)
   }))
+  output$plot1 = renderPlot({
+    data1 = mydata1
+    data1 = subset(data1,data1$城市==input$select1)
+    data1$录入日期 = as.Date(data1$录入日期,"%Y-%m-%d")
+    data1 = subset(data1,data1$录入日期 >= input$dates1[1] &
+                     data1$录入日期 <= input$dates1[2])
+    data1 = subset(data1,data1$委托类型==input$select2)
+    data1 = subset(data1,select=input$indicators1)
+    ggplot(data = data1,aes(录入日期,委托量,group=1)) + 
+      geom_line(color="#00AE66") +
+      labs(title="SE房源委托量",x="日期",y="量") +
+      theme_bw(base_family = "STHeiti")
+  })
+  output$plot2 = renderPlot({
+    data1 = mydata1
+    data1 = subset(data1,data1$城市==input$select1)
+    data1$录入日期 = as.Date(data1$录入日期,"%Y-%m-%d")
+    data1 = subset(data1,data1$录入日期 >= input$dates1[1] &
+                     data1$录入日期 <= input$dates1[2])
+    data1 = subset(data1,data1$委托类型==input$select2)
+    data1 = subset(data1,select=input$indicators1)
+    ggplot(data = data1,aes(录入日期,单位面积委托价格,group=1)) + 
+      geom_line(color="#00AE66") +
+      labs(title="SE房源单位面积委托价格",x="日期",y="量") +
+      theme_bw(base_family = "STHeiti")
+  })
+  output$plot3 = renderPlot({
+    data1 = mydata1
+    data1 = subset(data1,data1$城市==input$select1)
+    data1$录入日期 = as.Date(data1$录入日期,"%Y-%m-%d")
+    data1 = subset(data1,data1$录入日期 >= input$dates1[1] &
+                     data1$录入日期 <= input$dates1[2])
+    data1 = subset(data1,data1$委托类型==input$select2)
+    data1 = subset(data1,select=input$indicators1)
+    ggplot(data = data1,aes(录入日期,平均建筑面积,group=1)) + 
+      geom_line(color="#00AE66") +
+      labs(title="SE房源委托平均建筑面积",x="日期",y="量") +
+      theme_bw(base_family = "STHeiti")
+  })
 }
--- ui.R
+++ ui.R
@@ -29,13 +29,20 @@ sidebar = dashboardSidebar(
   sidebarMenu(id = "tabs",
               sidebarSearchForm(textId = "searchText",buttonId = "searchButton",
                                 label = "Search..."),
-              menuItem("宏观经济数据",tabName = "dashboard",icon = icon("dashboard"),
+              menuItem("宏观数据",tabName = "dashboard",icon = icon("dashboard"),
                        menuSubItem("整体宏观经济",tabName = "macro_1",icon = icon("angle-double-right")),
                        menuSubItem("北京",tabName = "macro_2",icon = icon("angle-double-right"))
               ),
-              menuItem("链家房地产数据",tabName = "lianjia_data",icon = icon("dashboard"),
-                       menuSubItem("SE房源",tabName = "se_secondhand_house",icon = icon("angle-double-right")),
-                       menuSubItem("新房",tabName = "new_house",icon = icon("angle-double-right"))
+              menuItem("链家数据",tabName = "lianjia_data",icon = icon("home"),
+                       menuSubItem("SE房源",tabName = "se_house",icon = icon("angle-double-right")),
+                       menuSubItem("SE客源",tabName = "se_customer",icon = icon("angle-double-right")),
+                       menuSubItem("SE成交",tabName = "se_constract",icon = icon("angle-double-right")),
+                       menuSubItem("SE调价",tabName = "se_changeprice",icon = icon("angle-double-right")),
+                       menuSubItem("链家新房",tabName = "new_house",icon = icon("angle-double-right"))
+              ),
+              menuItem("网签数据",tabName = "netconstract_data",icon = icon("globe"),
+                       menuSubItem("北京网签",tabName = "bj_netconstract",icon = icon("angle-double-right")),
+                       menuSubItem("大连网签",tabName = "dl_netconstract",icon = icon("angle-double-right"))
               ),
               menuItem("关于我们",tabName = "widgets",icon = icon("question"))
   )
@@ -44,18 +51,18 @@ sidebar = dashboardSidebar(
 body = dashboardBody(
   tabItems(
     tabItem(
-      tabName = "se_secondhand_house",
+      tabName = "se_house",
       fluidRow(
         column(width = 12,
                tabBox(width = 3,
                       tabPanel(h5("筛选条件"),
                                selectInput("select1",label = h5("选择城市"), 
-                                           choices = unique(mydata$城市), selected = "北京"),
+                                           choices = unique(mydata1$城市), selected = "北京"),
                                dateRangeInput("dates1",label = h5("选择日期"),language = "zh-CN"),
                                selectInput("select2",label = h5("委托类型"), 
-                                           choices = unique(mydata$委托类型), selected = "出售"),
+                                           choices = unique(mydata1$委托类型), selected = "出售"),
                                checkboxGroupInput("indicators1",label = h5("选择指标"),
-                                                  choices = names(mydata),
+                                                  choices = names(mydata1),
                                                   selected = c("城市","录入日期","委托类型","委托量")),
                                submitButton("确定")),
                       tabPanel(h5("下载"),
@@ -66,8 +73,12 @@ body = dashboardBody(
                ),
                tabBox(width = 9,
                       tabPanel(h5("报表"),
-                        DT::dataTableOutput('table')),
-                      tabPanel(h5("图形")),
+                        DT::dataTableOutput('table1')),
+                      tabPanel(h5("图形"),
+                        plotOutput('plot1',width = "70%",height = "350px"),
+                        plotOutput('plot2',width = "70%",height = "350px"),
+                        plotOutput('plot3',width = "70%",height = "350px")
+                        ),
                       tabPanel(h5("数据挖掘"))
                ))
     )
